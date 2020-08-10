114aa524a8496e7f18bbfd8122a1b3b8334ee153
Gaurav Thakur <gthakur581@gmail.com>
2020-08-10 10:21:19 +0530

    Updates

---
+++ precache-manifest.906d6860a5ec414f1d18fcb9e77da55f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8dc2e3794c86dc0380667849b6121c8",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "e5095f44687c64b4baf9",
+    "url": "/react-test/static/css/main.aafb6ede.chunk.css"
+  },
+  {
+    "revision": "8ff74ea6ac698aab018e",
+    "url": "/react-test/static/js/2.6b278ec3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test/static/js/2.6b278ec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5095f44687c64b4baf9",
+    "url": "/react-test/static/js/main.3150fbff.chunk.js"
+  },
+  {
+    "revision": "68d9c817fb5aeaf78725",
+    "url": "/react-test/static/js/runtime-main.5f6d1232.js"
+  }
+]);
